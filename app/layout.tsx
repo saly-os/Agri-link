00e608a56db7529f472924c07b1dbe1018fa704f
Sokhna Salimata Ogo Seck <Salimata@192.168.1.7>
2026-01-30 07:47:20 +0000

    Config SpeedInsight

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,3 +56,23 @@ export default function RootLayout({
     </html>
   )
 }
+
+import { SpeedInsights } from '@vercel/speed-insights/next';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
